78cac1326f298621168cedc105bf54a9beac58af
Brianna Kwan <kwanbnn11@gmail.com>
2021-08-05 14:00:15 -0400

    problem

--- Recycl-ED/InfoViewController.swift
+++ Recycl-ED/InfoViewController.swift
@@ -15,12 +15,34 @@ class InfoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    //move this to the petitions part
+    @IBAction func basics(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.epa.gov/recycle/recycling-basics")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func bottleBill(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.bottlebill.org/index.php/about-bottle-bills/what-is-a-bottle-bill")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func NYBill(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.dec.ny.gov/chemical/8500.html")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func PCC(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://yaleclimateconnections.org/2019/08/how-plastics-contribute-to-climate-change/")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func PWCC(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.wwf.org.au/news/blogs/plastic-waste-and-climate-change-whats-the-connection/")! as URL, options: [:], completionHandler: nil)
+    }
+    
     @IBAction func georgiaBill(_ sender: UIButton) {
         UIApplication.shared.open(URL(string:"https://www.change.org/p/nathan-deal-increase-recycling-in-georgia-by-passing-a-bottle-bill?source_location=topic_page")! as URL, options: [:], completionHandler: nil)
     }
     
-
+    @IBAction func easierRecycling(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.change.org/p/beverage-producers-make-recycling-easier?source_location=topic_page")! as URL, options: [:], completionHandler: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
