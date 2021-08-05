69bc3ec96b42cdd0f74a9f7717d41829f4d3cf08
Brianna Kwan <kwanbnn11@gmail.com>
2021-08-05 12:14:07 -0400

    some links

--- Recycl-ED/InfoViewController.swift
+++ Recycl-ED/InfoViewController.swift
@@ -15,7 +15,11 @@ class InfoViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    //move this to the petitions part
+    @IBAction func georgiaBill(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.change.org/p/nathan-deal-increase-recycling-in-georgia-by-passing-a-bottle-bill?source_location=topic_page")! as URL, options: [:], completionHandler: nil)
+    }
+    
 
     /*
     // MARK: - Navigation
