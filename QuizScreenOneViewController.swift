b8b91e32ca730c3463e372a7694b1d12fd71e217
Anna Chan <annac0068@gmail.com>
2021-08-05 23:38:25 -0400

    plant

--- QuizScreenOneViewController.swift
+++ QuizScreenOneViewController.swift
@@ -17,35 +17,43 @@ class QuizScreenOneViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        plant1.isHidden = false
+        plant1.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
   
+    @IBOutlet weak var plant0: UIImageView!
     @IBOutlet weak var plant1: UIImageView!
     
     @IBAction func ChoiceAButtonPressed(_ sender: UIButton) {
         GradeLabel.text = "0/4"
         GradeCaptionLabel.text = "Oh no! Try again"
         plant1.isHidden = true
-        
+        plant0.isHidden = false
     }
     
     @IBAction func ChoiceBButtonPressed(_ sender: UIButton) {
         GradeLabel.text = "0/4"
         GradeCaptionLabel.text = "Oh no! Try again"
         plant1.isHidden = true
+        plant0.isHidden = false
+
     }
     
     @IBAction func ChoiceCButtonPressed(_ sender: UIButton) {
         GradeLabel.text = "1/4"
         GradeCaptionLabel.text = "Congrats! Your plant has sprouted!"
         plant1.isHidden = false
+        plant0.isHidden = true
+
     }
     
     @IBAction func ChoiceDButtonPressed(_ sender: UIButton) {
         GradeLabel.text = "0/4"
         GradeCaptionLabel.text = "Oh no! Try again"
         plant1.isHidden = true
+        plant0.isHidden = false
+
     }
     
     
